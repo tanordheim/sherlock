ddcc1c704e4fa2c7bf4ef4fc47f70310fcc54284
Sebastian Kootz <skxxtz@gmail.com>
2025-01-08 04:20:28 +0100

    improving app construction

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use gio::prelude::*;
 use gtk4::gdk;
 use gtk4::{prelude::*, Application, ApplicationWindow, EventControllerKey};
-use gtk4_layer_shell::{Edge, Layer, LayerShell};
+use gtk4_layer_shell::{Layer, LayerShell};
 use once_cell::sync::Lazy;
 
 mod components;
@@ -11,30 +11,21 @@ use helpers::config_loader::{read_config, Config};
 static CONFIG: Lazy<Config> = Lazy::new(read_config);
 
 
-fn activate(application: &Application) {
-    // Initialize Layer
-    let mut window = ApplicationWindow::new(application);
+
+fn create_main_window(application: &Application)-> ApplicationWindow{
+    let window:ApplicationWindow = ApplicationWindow::builder()
+        .application(application)
+        .default_width(900)
+        .default_height(589)
+        .resizable(false)
+        .build();
+
     window.init_layer_shell();
     window.set_layer(Layer::Overlay);
-
-    window.set_default_size(900, 589);
-    window.set_resizable(false);
-
-    helpers::load_css();
-
-    let anchors = [
-        (Edge::Left, false),
-        (Edge::Right, false),
-        (Edge::Top, false),
-        (Edge::Bottom, false),
-    ];
-    for (anchor, state) in anchors {
-        window.set_anchor(anchor, state);
-    }
-
-    // Important to let keyevents pass through
-    let event_controller = EventControllerKey::new();
     window.set_keyboard_mode(gtk4_layer_shell::KeyboardMode::Exclusive);
+
+
+    let event_controller = EventControllerKey::new();
     event_controller.set_propagation_phase(gtk4::PropagationPhase::Capture);
     event_controller.connect_key_pressed(move |_, key, _, _| {
         match key {
@@ -45,10 +36,10 @@ fn activate(application: &Application) {
         }
         false.into()
     });
-    window.add_controller(event_controller);
 
-    window = components::views::search(window);
-    window.present();
+    window.add_controller(event_controller);
+    return window
+
 }
 
 fn main() {
@@ -56,7 +47,11 @@ fn main() {
     let application = Application::new(Some("com.skxxtz.sherlock"), Default::default());
 
     application.connect_activate(move |app| {
-        activate(app);
+        let mut window = create_main_window(&app);
+        window.show();
+
+        window = components::views::search(window);
+        helpers::load_css();
     });
 
     application.run();
