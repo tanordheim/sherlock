c2037b5ad0e709b099631bf9f37a7012dc0f921f
Sebastian Kootz <skxxtz@gmail.com>
2025-01-18 23:33:54 +0100

    improved documentation

---
+++ docs/config.md
@@ -0,0 +1,25 @@
+# Configuration File
+
+The configuration file for Sherlock is located at `~/.config/sherlock/config.toml`, unless specified otherwise. This file allows you to customize various parameters to tailor Sherlock to your needs. Below, we will explore the available options and their purposes.
+
+---
+
+## Debug Section `[debug]`
+
+| **Keyword**           | **Default** | **Explanation**                                                                 |
+|-----------------------|-------------|---------------------------------------------------------------------------------|
+| `try_suppress_errors` | `false`     | If set to `true`, errors and warnings will not be displayed when starting the app. |
+| `try_suppress_warnings` | `false`   | If set to `true`, only errors will trigger the error screen at startup, while warnings will be ignored. |
+
+---
+
+## Appearance Section `[appearance]`
+
+| **Keyword**     | **Default** | **Explanation**                                                                                                                 |
+|-----------------|-------------|-------------------------------------------------------------------------------------------------------------------------------|
+| `gsk_renderer`  | `"cairo"`   | Specifies the renderer used to display the Sherlock application. During testing, `cairo` showed the fastest startup times. You can also use `ngl`, `gl`, or `vulkan` based on your system's performance. |
+| `recolor_icons` | `false`     | Appends the `-symbolic` postfix to all icons, allowing them to be colorized. Note: not all icons have a symbolic version.       |
+| `icon_paths`    | `[]`        | Defines custom paths for the application to search for icons. This is useful for adding custom icons for commands or aliases through the `sherlockalias` file. |
+
+---
+
--- src/actions/commandlaunch.rs
+++ src/actions/commandlaunch.rs
@@ -1,37 +1,61 @@
 use std::process::Command;
 
-pub fn command_launch(exec: &str, keyword: &String) {
+use crate::loader::util::SherlockError;
+
+pub fn command_launch(exec: &str, keyword: &String) -> Result<(), SherlockError>{
     let exec = exec.replace("{keyword}", &keyword);
-    let commands: Vec<&str> = exec.split('&').collect();
+    let commands: Vec<&str> = exec.split("&").collect();
     let num_cmds = commands.len();
 
     for command in commands {
         if !command.is_empty() {
+            
             let mut parts = command.split_whitespace();
             let execute = parts.next().expect("No command found");
             let args: Vec<&str> = parts.collect();
 
             if num_cmds > 1 {
-                Command::new(execute)
-                    .args(&args)
-                    .spawn()
-                    .expect(&format!("Failed to execute command: {:?}", command));
-
+                let output = asynchronous_execution(execute, args)?;
+                println!("{}", output);
             } else {
                 // Synchronous execution (output)
-                let output = Command::new(execute)
-                    .args(&args)
-                    .output()
-                    .expect(&format!("Failed to execute command: {:?}", command));
-
-                if output.status.success() {
-                    println!("Command executed successfully!");
-                    println!("Output: {}", String::from_utf8_lossy(&output.stdout));
-                } else {
-                    eprintln!("Command failed with status: {}", output.status);
-                    eprintln!("Error: {}", String::from_utf8_lossy(&output.stderr));
-                }
+                synchronous_execution(execute, args)?;
             }
         }
     }
+    Ok(())
+}
+
+fn synchronous_execution(execute:&str, args: Vec<&str>)->Result<String, SherlockError>{
+    let output = Command::new(execute)
+        .args(&args)
+        .output()
+        .map_err(|e| SherlockError {
+            name: "Command Execute Error".to_string(),
+            message: format!("Failed to execute synchronous command."),
+            traceback: e.to_string(),
+        })?;
+
+    if output.status.success(){
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(SherlockError {
+            name: "Command Execute Error".to_string(),
+            message: format!("Failed to execute synchronous command."),
+            traceback: String::from_utf8_lossy(&output.stderr).to_string(),
+        })
+    }
+}
+fn asynchronous_execution(execute:&str, args: Vec<&str>)->Result<String, SherlockError>{
+    let async_command = Command::new(execute)
+        .args(&args)
+        .spawn()
+        .map_err(|e| SherlockError {
+            name: "Command Execute Error".to_string(),
+            message: format!("Failed to execute synchronous command."),
+            traceback: e.to_string(),
+        })?;
+
+    Ok(format!("{:?}", async_command)) 
+
 }
