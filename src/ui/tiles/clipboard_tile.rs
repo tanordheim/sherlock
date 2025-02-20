aec5f2956be6a4f8cb4c5fc194ebfbcb13bff2a1
Sebastian Kootz <skxxtz@gmail.com>
2025-02-20 21:55:35 +0100

    feat: implemented the ctrl shortcut visuals for the clipboard action

--- src/ui/tiles/clipboard_tile.rs
+++ src/ui/tiles/clipboard_tile.rs
@@ -13,7 +13,7 @@ impl Tile {
         keyword: &String,
     ) -> (i32, Vec<ListBoxRow>) {
         let mut results: Vec<ListBoxRow> = Default::default();
-        let mut index_ref = index;
+        let mut is_valid: i32 = 0;
 
         //TODO implement searchstring before clipboard content
         if clipboard_content.contains(keyword){
@@ -23,6 +23,13 @@ impl Tile {
             let title_obj: Label = builder.object("app-name").unwrap();
             let attr_holder: Box = builder.object("attrs-holder").unwrap();
             let launcher_type: Label = builder.object("launcher-type").unwrap();
+
+            if index < 5 {
+                let shortcut_holder: Box = builder.object("shortcut-holder").unwrap();
+                let shortcut: Label = builder.object("shortcut").unwrap();
+                shortcut_holder.set_visible(true);
+                shortcut.set_text(format!("ctrl + {}", index + 1).as_str());
+            }
 
             let mut name = "";
             let mut method = "";
@@ -38,35 +45,34 @@ impl Tile {
             let re_url_check = r"^(https?:\/\/)?(www.)?([\da-z\.-]+)\.([a-z]{2,6})([\/\w\.-]*)*\/?$";
             let re = Regex::new(re_url_check).unwrap();
             if let Some(captures) = re.captures(clipboard_content){
+                is_valid = 1;
                 name = "Clipboard Web-Search";
                 method = "web_launcher";
                 let main_domain = captures.get(3).map_or("", |m| m.as_str());
                 icon = known_pages.get(main_domain).map_or("google", |m| m);
-                println!("{}", main_domain);
-
-    
             }
 
+        
+            if is_valid  == 1{
+                if name.is_empty() {
+                    launcher_type.set_visible(false);
+                }
 
-            if name.is_empty() {
-                launcher_type.set_visible(false);
+                launcher_type.set_text(name);
+                title_obj.set_text(clipboard_content);
+                icon_obj.set_icon_name(Some(&icon));
+
+                let attrs: Vec<String> = vec![
+                    format!("{} | {}", "method", method),
+                    format!("{} | {}", "keyword", clipboard_content),
+                    format!("{} | {}", "engine", "plain"),
+                ];
+
+                insert_attrs(&attr_holder, attrs);
+                results.push(holder);
             }
-
-            launcher_type.set_text(name);
-            title_obj.set_text(clipboard_content);
-            icon_obj.set_icon_name(Some(&icon));
-
-            let attrs: Vec<String> = vec![
-                format!("{} | {}", "method", method),
-                format!("{} | {}", "keyword", clipboard_content),
-                format!("{} | {}", "engine", "plain"),
-            ];
-
-            insert_attrs(&attr_holder, attrs);
-            index_ref += 1;
-            results.push(holder);
         }
 
-        return (index_ref + 1, results);
+        return (index + is_valid, results);
     }
 }
