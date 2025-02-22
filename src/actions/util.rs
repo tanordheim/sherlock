04888bffeba8ccbc3bf3fa5b943fa74998fccbad
Sebastian Kootz <skxxtz@gmail.com>
2025-02-23 00:56:51 +0100

    feat: implemented error handling for clipboard actions

--- src/actions/util.rs
+++ src/actions/util.rs
@@ -1,10 +1,21 @@
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
 
-pub fn copy_to_clipboard(string: &String) {
-    let mut ctx = ClipboardContext::new().unwrap();
+use crate::loader::util::SherlockError;
+
+pub fn copy_to_clipboard(string: &String)-> Result<(), SherlockError>{
+    let mut ctx = ClipboardContext::new().map_err(|e| SherlockError {
+        name: "ClipboardError".to_string(),
+        message: "Failed to get system clipboard".to_string(),
+        traceback: e.to_string(),
+    })?;
     ctx.set_contents(string.to_owned()).unwrap();
+    Ok(())
 }
-pub fn read_from_clipboard() -> String {
-    let mut ctx = ClipboardContext::new().unwrap();
-    ctx.get_contents().unwrap_or("".to_string())
+pub fn read_from_clipboard() -> Result<String, SherlockError>{
+    let mut ctx = ClipboardContext::new().map_err(|e| SherlockError {
+        name: "ClipboardError".to_string(),
+        message: "Failed to get system clipboard".to_string(),
+        traceback: e.to_string(),
+    })?;
+    Ok(ctx.get_contents().unwrap_or("".to_string()))
 }
