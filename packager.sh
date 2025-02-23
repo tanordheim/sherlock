c834c317a701d61c194071315e9c25dcbe63d175
Sebastian Kootz <skxxtz@gmail.com>
2025-02-23 22:05:23 +0100

    impr: improved packageer script

--- packager.sh
+++ packager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "Current version: " version
-
+rm -rf ~/.tmp/sherlock-release/
 mkdir -p ~/.tmp/sherlock-release/
 cargo build --release
 cp target/release/sherlock ~/.tmp/sherlock-release/sherlock
