2e5c6f5a54a81e4c4c3c3d97bacf55de4934bba0
Sebastian Kootz <skxxtz@gmail.com>
2025-02-24 15:06:23 +0100

    impr: implemented new sherlockerror notation

--- src/loader/resource_loader.rs
+++ src/loader/resource_loader.rs
@@ -1,6 +1,6 @@
 use gio::glib;
 
-use super::{util::SherlockError, Loader};
+use super::{util::{SherlockError, SherlockErrorType}, Loader};
 
 impl Loader {
     pub fn load_resources() -> Result<(), SherlockError> {
@@ -8,8 +8,7 @@ impl Loader {
         let resource =
             gio::Resource::from_data(&glib::Bytes::from_static(res_bytes)).map_err(|e| {
                 SherlockError {
-                    name: "Resource Parsing Error".to_string(),
-                    message: format!("Failed to parse gresources."),
+                    error: SherlockErrorType::ResourceParseError(),
                     traceback: e.to_string(),
                 }
             })?;
