2e9b0e5a10bf5610e4da3ed5c0dd035015f5c9d8
Sebastian Kootz <skxxtz@gmail.com>
2025-02-24 16:08:35 +0100

    impr: implemented new sherlockerror notation

--- src/actions/util.rs
+++ src/actions/util.rs
@@ -1,11 +1,10 @@
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
 
-use crate::loader::util::SherlockError;
+use crate::loader::util::{SherlockError, SherlockErrorType};
 
 pub fn copy_to_clipboard(string: &str) -> Result<(), SherlockError> {
     let mut ctx = ClipboardContext::new().map_err(|e| SherlockError {
-        name: "ClipboardError".to_string(),
-        message: "Failed to get system clipboard".to_string(),
+        error: SherlockErrorType::ClipboardError,
         traceback: e.to_string(),
     })?;
     let _ = ctx.set_contents(string.to_string());
@@ -13,8 +12,7 @@ pub fn copy_to_clipboard(string: &str) -> Result<(), SherlockError> {
 }
 pub fn read_from_clipboard() -> Result<String, SherlockError> {
     let mut ctx = ClipboardContext::new().map_err(|e| SherlockError {
-        name: "ClipboardError".to_string(),
-        message: "Failed to get system clipboard".to_string(),
+        error: SherlockErrorType::ClipboardError,
         traceback: e.to_string(),
     })?;
     Ok(ctx.get_contents().unwrap_or("".to_string()))
