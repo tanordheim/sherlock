2be84dc874c838217d2df7838da489bea465af5e
Sebastian Kootz <skxxtz@gmail.com>
2025-03-27 00:04:00 +0100

    fix: switched from before_exec to pre_exec due to deprecation

--- src/actions/commandlaunch.rs
+++ src/actions/commandlaunch.rs
@@ -22,15 +22,17 @@ pub fn command_launch(exec: &str, keyword: &str) -> Result<(), SherlockError> {
 
 fn asynchronous_execution(execute: &str, args: Vec<&str>) -> Result<(), SherlockError> {
     let mut command = Command::new(execute);
-    command
-        .args(args)
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .before_exec(|| {
-            nix::unistd::setsid().ok();
-            Ok(())
-        });
+    unsafe {
+        command
+            .args(args)
+            .stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .pre_exec(|| {
+                nix::unistd::setsid().ok();
+                Ok(())
+            });
+    }
 
     command.spawn().map_err(|e| SherlockError {
         error: SherlockErrorType::CommandExecutionError(execute.to_string()),
