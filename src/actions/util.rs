1822a6ec2da75b045ed8c5640c0390c99f085875
Sebastian Kootz <skxxtz@gmail.com>
2025-04-16 02:11:29 +0200

    impr: added util functions for cache cleaning

--- src/actions/util.rs
+++ src/actions/util.rs
@@ -1,6 +1,8 @@
+use std::fs;
+
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
 
-use crate::loader::util::{SherlockError, SherlockErrorType};
+use crate::{loader::util::{home_dir, SherlockError, SherlockErrorType}, CONFIG};
 
 pub fn copy_to_clipboard(string: &str) -> Result<(), SherlockError> {
     let mut ctx = ClipboardContext::new().map_err(|e| SherlockError {
@@ -17,4 +19,35 @@ pub fn read_from_clipboard() -> Result<String, SherlockError> {
         traceback: e.to_string(),
     })?;
     Ok(ctx.get_contents().unwrap_or_default().trim().to_string())
+}
+
+
+pub fn clear_cached_files()->Result<(), SherlockError>{
+    let config = CONFIG.get().ok_or_else(|| SherlockError{
+        error: SherlockErrorType::ConfigError(None),
+        traceback: String::from("Location: src/actions/util.rs")
+    })?;
+    let home = home_dir()?;
+    
+    // Clear app cache
+    fs::remove_file(&config.behavior.cache).map_err(|e| SherlockError {
+        error: SherlockErrorType::FileRemoveError(config.behavior.cache.clone()),
+        traceback: e.to_string()
+    })?;
+
+    // Clear sherlocks cache
+    fs::remove_dir_all(home.join(".cache/sherlock")).map_err(|e| SherlockError {
+        error: SherlockErrorType::DirRemoveError(String::from("~/.cache/sherlock")),
+        traceback: e.to_string()
+    })?;
+
+    Ok(())
+}
+
+pub fn reset_app_counter()->Result<(), SherlockError>{
+    let home = home_dir()?;
+    fs::remove_file(home.join(".sherlock/counts.json")).map_err(|e| SherlockError {
+        error: SherlockErrorType::FileRemoveError(home.join(".sherlock/counts.json")),
+        traceback: e.to_string()
+    })
 }
