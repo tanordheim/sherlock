f934594dcb9f78c2fe9d5654c5416cb204285ef5
Sebastian Kootz <skxxtz@gmail.com>
2025-05-05 20:51:29 +0200

    chore: version setup

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
       perSystem = {system, ...}: let
         name = "sherlock";
-        version = "0.1.11";
+        version = "0.1.12";
 
         pkgs = import nixpkgs {
           inherit system;
