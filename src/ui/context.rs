913889d8b48be6d07501ba1a4c23b93515726e8f
Sebastian Kootz <skxxtz@gmail.com>
2025-05-08 00:18:50 +0200

    impr: added fade-in animation for context menu

--- src/ui/context.rs
+++ src/ui/context.rs
@@ -5,34 +5,49 @@ use gio::{
 };
 use gtk4::{
     prelude::{ListItemExt, WidgetExt},
-    ListView, SignalListItemFactory, SingleSelection,
+    ListView, Revealer, SignalListItemFactory, SingleSelection,
 };
 
-use crate::g_subclasses::action_entry::ContextAction;
+use crate::{g_subclasses::action_entry::ContextAction, CONFIG};
 
-pub fn make_context() -> (ListView, ListStore) {
+pub fn make_context() -> (ListView, ListStore, Revealer) {
     let factory = make_factory();
     let model = ListStore::new::<ContextAction>();
     let selection = SingleSelection::new(Some(model.clone()));
     let context = ListView::new(Some(selection), Some(factory));
 
+    let revealer = Revealer::builder()
+        .transition_type(gtk4::RevealerTransitionType::Crossfade)
+        .transition_duration(100)
+        .child(&context)
+        .build();
+    if !CONFIG.get().map_or(false, |c| c.behavior.animate) {
+        revealer.set_transition_duration(0);
+    }
+
     model.connect_items_changed({
-        let context_clone = context.downgrade();
+        let revealer = revealer.downgrade();
         move |model, _, _, _| {
-            if let Some(context) = context_clone.upgrade() {
-                let n_items = model.n_items();
-                context.set_visible(n_items != 0);
+            let n_items = model.n_items();
+            if let Some(revealer) = revealer.upgrade() {
+                if n_items != 0 {
+                    revealer.set_reveal_child(true);
+                } else {
+                    let tmp = revealer.transition_duration();
+                    revealer.set_transition_duration(0);
+                    revealer.set_reveal_child(false);
+                    revealer.set_transition_duration(tmp);
+                }
             }
         }
     });
 
     context.set_widget_name("context-menu");
     context.set_focusable(false);
-    context.set_visible(false);
     context.set_width_request(300);
     context.set_halign(gtk4::Align::End);
     context.set_valign(gtk4::Align::End);
-    (context, model)
+    (context, model, revealer)
 }
 fn make_factory() -> SignalListItemFactory {
     let factory = SignalListItemFactory::new();
